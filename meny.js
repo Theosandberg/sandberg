6d63f159cad6117711ae98bf04c49adb8c6305f6
Theosandberg <47246482+Theosandberg@users.noreply.github.com>
2019-02-01 15:16:48 +0100

    Add files via upload

---
+++ meny.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+$(document).ready(function(){
+	var scrollValue = 0;
+	
+	$(window).scroll(function(){
+		var curValue = $(this).scrollTop();
+		if(curValue > scrollValue)
+	{
+	var i = 2;
+	removeLetter();
+	function removeLetter()
+	{
+		$("#l"+i).fadeOut(0);
+		if(i < 9)
+		{
+			i++;
+			setTimeout(removeLetter,30);
+		}
+	}
+}
+else {
+	var i = 13;
+	showLetter();
+	function showLetter()
+	{
+		$("#l"+i).fadeIn(0);
+		if(i >= 2)
+		{
+			i--;
+			setTimeout(showLetter,30);
+		}
+	}
+}
+scrollValue = curValue;
+});
+});
